e1ba7bb511ee478b466b0f538809f17e9df23cd8
example <example@example.com>
2023-03-07 02:03:19 -0800

    Organize application with package

--- .flaskenv
+++ .flaskenv
@@ -1,1 +1,2 @@
-FLASK_ENV=development
+FLASK_ENV=developmentFLASK_ENV=development
+FLASK_APP=watchlist
--- test_watchlist.py
+++ test_watchlist.py
@@ -1,7 +1,8 @@
-# -*- coding: utf-8 -*-
 import unittest
 
-from app import app, db, Movie, User, forge, initdb
+from watchlist import app, db
+from watchlist.models import Movie, User
+from watchlist.commands import forge, initdb
 
 
 class WatchlistTestCase(unittest.TestCase):
